1f187f72df5b18288a81530b9f75272ff934194e
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-03-22 20:16:29 -0300

    updating java version to 8

--- src/main/java/com/example/calc/CalculadoraServerPublisher.java
+++ src/main/java/com/example/calc/CalculadoraServerPublisher.java
@@ -6,7 +6,8 @@ public class CalculadoraServerPublisher {
 
 	public static void main(String[] args) {
 
-		 Endpoint.publish("http://127.0.0.1:9876/calc", new CalculadoraServerImpl());
+		 Endpoint.publish("http://127.0.0.1:9876/calc", 
+		 new CalculadoraServerImpl());
 
 	}
 
